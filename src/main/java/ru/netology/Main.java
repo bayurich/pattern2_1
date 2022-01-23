762f7f6ed3cddc28bd2b916bc5510f74d184c01d
Aleks <beilis@mail.ru>
2022-01-23 20:28:04 +0500

    add project

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+
+        System.out.println(calc.sum(2, 2));
+        System.out.println(calc.sum(10, 22));
+        System.out.println(calc.mult(10, 33));
+        System.out.println(calc.pow(2, 10));
+        System.out.println(calc.pow(3, 2));
+    }
+}
